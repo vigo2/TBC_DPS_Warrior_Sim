6f77b6d296b00c4ad218d47401e43a5f4c389cc3
The Grox Empire <47816228+TheGroxEmpire@users.noreply.github.com>
2021-10-27 14:30:47 +0700

    Hotfix: Rating decimals fix follow-up

--- wow_library/source/Armory.cpp
+++ wow_library/source/Armory.cpp
@@ -148,11 +148,11 @@ Special_stats Armory::get_enchant_special_stats(Socket socket, Enchant::Type typ
         case Enchant::Type::attack_power:
             return {0, 0, 30};
         case Enchant::Type::naxxramas:
-            return {0.63, 0, 26};
+            return {0.634057971, 0, 26};
         case Enchant::Type::greater_vengeance:
-            return {0.45, 0, 30};
+            return {0.4528985507, 0, 30};
         case Enchant::Type::greater_blade:
-            return {0.67, 0, 20};
+            return {0.6793478261, 0, 20};
         default:
             return {0, 0, 0};
         }
@@ -174,9 +174,9 @@ Special_stats Armory::get_enchant_special_stats(Socket socket, Enchant::Type typ
         case Enchant::Type::haste:
             return {0, 0, 0, 0, .0063};
         case Enchant::Type::cobrahide:
-            return {0.45, 0, 40};
+            return {0.4528985507, 0, 40};
         case Enchant::Type::nethercobra:
-            return {0.54, 0, 50};
+            return {0.5434782609, 0, 50};
         default:
             return {0, 0, 0};
         }
