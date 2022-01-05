a1191c7b5317b22224cdf7bf8724409c8536ed2c
The Grox Empire <47816228+TheGroxEmpire@users.noreply.github.com>
2022-01-05 11:57:08 +0700

    Local test fix

--- common/tests/test_common.cpp
+++ common/tests/test_common.cpp
@@ -5,43 +5,43 @@
 TEST(TestSuite, test_find_value_class)
 {
     std::vector<std::string> mult_armor_vec;
-    mult_armor_vec.emplace_back("lionheart_helm");
-    mult_armor_vec.emplace_back("stormrages_talisman_of_seething");
-    mult_armor_vec.emplace_back("conquerors_spaulders");
-    mult_armor_vec.emplace_back("shroud_of_dominion");
-    mult_armor_vec.emplace_back("plated_abomination_ribcage");
-    mult_armor_vec.emplace_back("hive_defiler_wristguards");
-    mult_armor_vec.emplace_back("gauntlets_of_annihilation");
-    mult_armor_vec.emplace_back("girdle_of_the_mentor");
-    mult_armor_vec.emplace_back("legplates_of_carnage");
-    mult_armor_vec.emplace_back("chromatic_boots");
+    mult_armor_vec.emplace_back("destroyer_battlehelm");
+    mult_armor_vec.emplace_back("pendant_of_the_perilous");
+    mult_armor_vec.emplace_back("destroyer_shoulderblades");
+    mult_armor_vec.emplace_back("thalassian_wildercloak");
+    mult_armor_vec.emplace_back("bulwark_of_ancient_kings");
+    mult_armor_vec.emplace_back("bracers_of_eradication");
+    mult_armor_vec.emplace_back("destroyer_gauntlets");
+    mult_armor_vec.emplace_back("belt_of_one_hundred_deaths");
+    mult_armor_vec.emplace_back("destroyer_greaves");
+    mult_armor_vec.emplace_back("warboots_of_obliteration");
 
     {
         std::vector<size_t> values{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         Find_values<size_t> fv(mult_armor_vec, values);
-        EXPECT_TRUE(fv.find("lionheart_helm") == 0);
-        EXPECT_TRUE(fv.find("stormrages_talisman_of_seething") == 1);
-        EXPECT_TRUE(fv.find("conquerors_spaulders") == 2);
-        EXPECT_TRUE(fv.find("shroud_of_dominion") == 3);
-        EXPECT_TRUE(fv.find("plated_abomination_ribcage") == 4);
-        EXPECT_TRUE(fv.find("hive_defiler_wristguards") == 5);
-        EXPECT_TRUE(fv.find("gauntlets_of_annihilation") == 6);
-        EXPECT_TRUE(fv.find("girdle_of_the_mentor") == 7);
-        EXPECT_TRUE(fv.find("legplates_of_carnage") == 8);
-        EXPECT_TRUE(fv.find("chromatic_boots") == 9);
+        EXPECT_TRUE(fv.find("destroyer_battlehelm") == 0);
+        EXPECT_TRUE(fv.find("pendant_of_the_perilous") == 1);
+        EXPECT_TRUE(fv.find("destroyer_shoulderblades") == 2);
+        EXPECT_TRUE(fv.find("thalassian_wildercloak") == 3);
+        EXPECT_TRUE(fv.find("bulwark_of_ancient_kings") == 4);
+        EXPECT_TRUE(fv.find("bracers_of_eradication") == 5);
+        EXPECT_TRUE(fv.find("destroyer_gauntlets") == 6);
+        EXPECT_TRUE(fv.find("belt_of_one_hundred_deaths") == 7);
+        EXPECT_TRUE(fv.find("destroyer_greaves") == 8);
+        EXPECT_TRUE(fv.find("warboots_of_obliteration") == 9);
     }
     {
         std::vector<double> values{0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0};
         Find_values<double> fv(mult_armor_vec, values);
-        EXPECT_TRUE(fv.find("lionheart_helm") == 0.0);
-        EXPECT_TRUE(fv.find("stormrages_talisman_of_seething") == 1.0);
-        EXPECT_TRUE(fv.find("conquerors_spaulders") == 2.0);
-        EXPECT_TRUE(fv.find("shroud_of_dominion") == 3.0);
-        EXPECT_TRUE(fv.find("plated_abomination_ribcage") == 4.0);
-        EXPECT_TRUE(fv.find("hive_defiler_wristguards") == 5.0);
-        EXPECT_TRUE(fv.find("gauntlets_of_annihilation") == 6.0);
-        EXPECT_TRUE(fv.find("girdle_of_the_mentor") == 7.0);
-        EXPECT_TRUE(fv.find("legplates_of_carnage") == 8.0);
-        EXPECT_TRUE(fv.find("chromatic_boots") == 9.0);
+        EXPECT_TRUE(fv.find("destroyer_battlehelm") == 0.0);
+        EXPECT_TRUE(fv.find("pendant_of_the_perilous") == 1.0);
+        EXPECT_TRUE(fv.find("destroyer_shoulderblades") == 2.0);
+        EXPECT_TRUE(fv.find("thalassian_wildercloak") == 3.0);
+        EXPECT_TRUE(fv.find("bulwark_of_ancient_kings") == 4.0);
+        EXPECT_TRUE(fv.find("bracers_of_eradication") == 5.0);
+        EXPECT_TRUE(fv.find("destroyer_gauntlets") == 6.0);
+        EXPECT_TRUE(fv.find("belt_of_one_hundred_deaths") == 7.0);
+        EXPECT_TRUE(fv.find("destroyer_greaves") == 8.0);
+        EXPECT_TRUE(fv.find("warboots_of_obliteration") == 9.0);
     }
 }
--- item_optimizer/tests/test_item_optimizer.cpp
+++ item_optimizer/tests/test_item_optimizer.cpp
@@ -1,118 +1,118 @@
-// #include "item_optimizer_fixture.cpp"
-// #include "string_helpers.hpp"
+#include "item_optimizer_fixture.cpp"
+#include "string_helpers.hpp"
 
-// TEST_F(Optimizer_fixture, test_multi_select)
-// {
-//     std::vector<std::string> mult_armor_vec{};
-//     mult_armor_vec.emplace_back("lionheart_helm");
-//     mult_armor_vec.emplace_back("mask_of_the_unforgiven");
+TEST_F(Optimizer_fixture, test_multi_select)
+{
+    std::vector<std::string> mult_armor_vec{};
+    mult_armor_vec.emplace_back("destroyer_battlehelm");
+    mult_armor_vec.emplace_back("warbringer_greathelm");
 
-//     mult_armor_vec.emplace_back("stormrages_talisman_of_seething");
-//     mult_armor_vec.emplace_back("mark_of_fordring");
+    mult_armor_vec.emplace_back("pendant_of_the_perilous");
+    mult_armor_vec.emplace_back("braided_eternium_chain");
 
-//     mult_armor_vec.emplace_back("drake_talon_pauldrons");
-//     mult_armor_vec.emplace_back("spaulders_of_valor");
+    mult_armor_vec.emplace_back("warbringer_shoulderplates");
+    mult_armor_vec.emplace_back("destroyer_shoulderblades");
 
-//     mult_armor_vec.emplace_back("cloak_of_draconic_might");
-//     mult_armor_vec.emplace_back("cape_of_the_black_baron");
+    mult_armor_vec.emplace_back("thalassian_wildercloak");
+    mult_armor_vec.emplace_back("vengeance_wrap");
 
-//     mult_armor_vec.emplace_back("plated_abomination_ribcage");
-//     mult_armor_vec.emplace_back("breastplate_of_annihilation");
+    mult_armor_vec.emplace_back("bulwark_of_kings");
+    mult_armor_vec.emplace_back("bulwark_of_ancient_kings");
 
-//     mult_armor_vec.emplace_back("hive_defiler_wristguards");
-//     mult_armor_vec.emplace_back("bracers_of_brutality");
+    mult_armor_vec.emplace_back("bladespire_warbands");
+    mult_armor_vec.emplace_back("bracers_of_eradication");
 
-//     mult_armor_vec.emplace_back("gauntlets_of_heroism");
-//     mult_armor_vec.emplace_back("gauntlets_of_valor");
+    mult_armor_vec.emplace_back("gauntlets_of_martial_perfection");
+    mult_armor_vec.emplace_back("destroyer_gauntlets");
 
-//     mult_armor_vec.emplace_back("onslaught_girdle");
-//     mult_armor_vec.emplace_back("brigam_girdle");
+    mult_armor_vec.emplace_back("belt_of_one_hundred_deaths");
+    mult_armor_vec.emplace_back("girdle_of_the_endless_pit");
 
-//     std::vector<std::string> mult_weapon_vec{};
-//     mult_weapon_vec.emplace_back("mirahs_song");
-//     mult_weapon_vec.emplace_back("chromatically_tempered_sword");
+    std::vector<std::string> mult_weapon_vec{};
+    mult_weapon_vec.emplace_back("gladiators_slicer");
+    mult_weapon_vec.emplace_back("merciless_gladiators_slicer");
 
-//     std::vector<extra_option> extra_options{};
-//     extra_options.emplace_back("item_filter_threshold", 2500);
-//     extra_options.emplace_back("item_filter_timeout", 1);
-//     extra_options.emplace_back("min_iterations", 15);
+    std::vector<extra_option> extra_options{};
+    extra_options.emplace_back("item_filter_threshold", 2500);
+    extra_options.emplace_back("item_filter_timeout", 1);
+    extra_options.emplace_back("min_iterations", 15);
 
-//     set_multiselect_fixture(mult_armor_vec, mult_weapon_vec, extra_options);
-//     set_target_items({"lionheart_helm", "stormrages_talisman_of_seething", "drake_talon_pauldrons",
-//                       "cloak_of_draconic_might", "plated_abomination_ribcage", "hive_defiler_wristguards",
-//                       "gauntlets_of_heroism"});
+    set_multiselect_fixture(mult_armor_vec, mult_weapon_vec, extra_options);
+    set_target_items({"destroyer_battlehelm", "pendant_of_the_perilous", "destroyer_shoulderblades",
+                      "thalassian_wildercloak","bulwark_of_ancient_kings", "bracers_of_eradication", 
+                      "destroyer_gauntlets"});
 
-//     double success_rate = run_multiselect(10, true);
+    double success_rate = run_multiselect(10, true);
 
-//     // Allow two failed runs. This is to make it very unlikely to fail if it is working.
-//     EXPECT_GE(success_rate, 0.8);
-// }
+    // Allow two failed runs. This is to make it very unlikely to fail if it is working.
+    EXPECT_GE(success_rate, 0.8);
+}
 
-// TEST_F(Optimizer_fixture, test_multi_select_item_filter)
-// {
-//     std::vector<std::string> mult_armor_vec{};
-//     mult_armor_vec.emplace_back("lionheart_helm");
-//     mult_armor_vec.emplace_back("mask_of_the_unforgiven");
+TEST_F(Optimizer_fixture, test_multi_select_item_filter)
+{
+    std::vector<std::string> mult_armor_vec{};
+    mult_armor_vec.emplace_back("destroyer_battlehelm");
+    mult_armor_vec.emplace_back("warbringer_greathelm");
 
-//     mult_armor_vec.emplace_back("stormrages_talisman_of_seething");
-//     mult_armor_vec.emplace_back("mark_of_fordring");
+    mult_armor_vec.emplace_back("pendant_of_the_perilous");
+    mult_armor_vec.emplace_back("braided_eternium_chain");
 
-//     mult_armor_vec.emplace_back("drake_talon_pauldrons");
-//     mult_armor_vec.emplace_back("spaulders_of_valor");
+    mult_armor_vec.emplace_back("warbringer_shoulderplates");
+    mult_armor_vec.emplace_back("destroyer_shoulderblades");
 
-//     mult_armor_vec.emplace_back("cloak_of_draconic_might");
-//     mult_armor_vec.emplace_back("cape_of_the_black_baron");
+    mult_armor_vec.emplace_back("thalassian_wildercloak");
+    mult_armor_vec.emplace_back("vengeance_wrap");
 
-//     mult_armor_vec.emplace_back("plated_abomination_ribcage");
-//     mult_armor_vec.emplace_back("breastplate_of_annihilation");
+    mult_armor_vec.emplace_back("bulwark_of_kings");
+    mult_armor_vec.emplace_back("bulwark_of_ancient_kings");
 
-//     mult_armor_vec.emplace_back("hive_defiler_wristguards");
-//     mult_armor_vec.emplace_back("bracers_of_brutality");
+    mult_armor_vec.emplace_back("bladespire_warbands");
+    mult_armor_vec.emplace_back("bracers_of_eradication");
 
-//     mult_armor_vec.emplace_back("gauntlets_of_heroism");
-//     mult_armor_vec.emplace_back("gauntlets_of_valor");
+    mult_armor_vec.emplace_back("gauntlets_of_martial_perfection");
+    mult_armor_vec.emplace_back("destroyer_gauntlets");
 
-//     mult_armor_vec.emplace_back("onslaught_girdle");
-//     mult_armor_vec.emplace_back("brigam_girdle");
+    mult_armor_vec.emplace_back("belt_of_one_hundred_deaths");
+    mult_armor_vec.emplace_back("girdle_of_the_endless_pit");
 
-//     mult_armor_vec.emplace_back("cloudkeeper_legplates");
-//     mult_armor_vec.emplace_back("eldritch_legplates");
+    mult_armor_vec.emplace_back("destroyer_greaves");
+    mult_armor_vec.emplace_back("warbringer_greaves");
 
-//     mult_armor_vec.emplace_back("chromatic_boots");
-//     mult_armor_vec.emplace_back("slime_kickers");
+    mult_armor_vec.emplace_back("warboots_of_obliteration");
+    mult_armor_vec.emplace_back("ironstriders_of_urgency");
 
-//     mult_armor_vec.emplace_back("quick_strike_ring");
-//     mult_armor_vec.emplace_back("master_dragonslayers_ring");
-//     mult_armor_vec.emplace_back("blackstone_ring");
-//     mult_armor_vec.emplace_back("tarnished_elven_ring");
+    mult_armor_vec.emplace_back("band_of_the_ranger_general");
+    mult_armor_vec.emplace_back("mithril_band_of_the_unscarred");
+    mult_armor_vec.emplace_back("shapeshifters_signet");
+    mult_armor_vec.emplace_back("veterans_band_of_triumph");
 
-//     mult_armor_vec.emplace_back("mark_of_the_champion");
-//     mult_armor_vec.emplace_back("kiss_of_the_spider");
-//     mult_armor_vec.emplace_back("blackhands_breadth");
+    mult_armor_vec.emplace_back("mark_of_the_champion");
+    mult_armor_vec.emplace_back("dragonspine_trophy");
+    mult_armor_vec.emplace_back("tsunami_talisman");
 
-//     mult_armor_vec.emplace_back("larvae_of_the_great_worm");
-//     mult_armor_vec.emplace_back("blastershot");
+    mult_armor_vec.emplace_back("serpent_spine_longbow");
+    mult_armor_vec.emplace_back("mamas_insurance");
 
-//     std::vector<std::string> mult_weapon_vec{};
-//     mult_weapon_vec.emplace_back("mirahs_song");
-//     mult_weapon_vec.emplace_back("chromatically_tempered_sword");
-//     mult_weapon_vec.emplace_back("crul_shorukh_edge_of_chaos");
-//     mult_weapon_vec.emplace_back("dooms_edge");
+    std::vector<std::string> mult_weapon_vec{};
+    mult_weapon_vec.emplace_back("gladiators_slicer");
+    mult_weapon_vec.emplace_back("merciless_gladiators_slicer");
+    mult_weapon_vec.emplace_back("talon_of_azshara");
+    mult_weapon_vec.emplace_back("gladiators_quickblade");
 
-//     std::vector<extra_option> extra_options{};
-//     extra_options.emplace_back("item_filter_threshold", 200);
-//     extra_options.emplace_back("item_filter_timeout", 0.5);
-//     extra_options.emplace_back("min_iterations", 15);
+    std::vector<extra_option> extra_options{};
+    extra_options.emplace_back("item_filter_threshold", 200);
+    extra_options.emplace_back("item_filter_timeout", 0.5);
+    extra_options.emplace_back("min_iterations", 15);
 
-//     set_multiselect_fixture(mult_armor_vec, mult_weapon_vec, extra_options);
-//     set_target_items({"lionheart_helm", "stormrages_talisman_of_seething", "drake_talon_pauldrons",
-//                       "cloak_of_draconic_might", "plated_abomination_ribcage", "hive_defiler_wristguards",
-//                       "gauntlets_of_heroism", "onslaught_girdle", "cloudkeeper_legplates", "chromatic_boots",
-//                       "quick_strike_ring", "master_dragonslayers_ring", "mark_of_the_champion", "kiss_of_the_spider",
-//                       "larvae_of_the_great_worm", "crul_shorukh_edge_of_chaos", "dooms_edge"});
+    set_multiselect_fixture(mult_armor_vec, mult_weapon_vec, extra_options);
+    set_target_items({"destroyer_battlehelm", "pendant_of_the_perilous", "destroyer_shoulderblades",
+                      "bulwark_of_ancient_kings", "bracers_of_eradication", "destroyer_gauntlets",
+                      "belt_of_one_hundred_deaths", "destroyer_greaves", "warboots_of_obliteration",
+                      "band_of_the_ranger_general", "veterans_band_of_triumph", "tsunami_talisman", "mark_of_the_champion",
+                      "mamas_insurance", "merciless_gladiators_slicer", "talon_of_azshara"});
 
-//     double success_rate = run_multiselect(10, true);
+    double success_rate = run_multiselect(10, true);
 
-//     // Allow two failed runs. This is to make it very unlikely to fail if it is working.
-//     EXPECT_GE(success_rate, 0.8);
-// }
+    // Allow two failed runs. This is to make it very unlikely to fail if it is working.
+    EXPECT_GE(success_rate, 0.8);
+}
